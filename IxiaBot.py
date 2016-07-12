7d91585d4c6af985b4e936ac397f77d9aeea473c
Hydrox6 <ikada@protonmail.ch>
2016-07-12 05:25:50 +0100

    Whispers work!
    It's like a dream come true! IxiaBot finally responds to chat and commands!
    Next up is command limiting, so she doesn't get shadowbanned. Also, a command system, with access control.

--- IxiaBot.py
+++ IxiaBot.py
@@ -1,7 +1,9 @@
 import socket,re,time,datetime,json,urllib
+from threading import Thread
 import TwitchUtils,IxiaExceptions
 
 msg = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+whisper = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv WHISPER \w+ :")
 
 class IxiaBot:
 
@@ -10,10 +12,16 @@ class IxiaBot:
         self.token = token
         self.channel = channel
         self.socket = TwitchUtils.connectToChannel(self.token, self.nick, self.channel)
+        self.wsocket = TwitchUtils.setupWhispers(self.token,self.nick)
+        self.running = True
+
+    def start(self):
+        Thread(None,self.whisperListen,self.nick+"-Whispers",()).start()
+        self.listen()
 
     def listen(self):
         incomplete = ""
-        while True:
+        while self.running:
             last = self.socket.recv(128).decode("utf-8") # Reads 128 Bytes from the chat
             recents = last.split("\r\n") # Splits it up by line(s)
             recents[0] = incomplete + recents[0] # Adds on the previous incomplete line.
@@ -26,12 +34,51 @@ class IxiaBot:
                 if line[:19] == "PING :tmi.twitch.tv":
                     self.socket.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
                 else:
-                    if not oline[:14] == ":tmi.twitch.tv" and not oline == u"" and not line == oline:
+                    if not sender == self.nick and not oline[:14] == ":tmi.twitch.tv" and not oline == u"" and not line == oline:
                         if "greetings" in line:
                             self.chat("ohaider")
                         try:
                             print(sender, "-", line)
                         except UnicodeError:
                             print("~~## ERROR LINE =( ##~~")
+        time.sleep(0.01)
+
+    def whisperListen(self):
+        incomplete = ""
+        print("Starting whisper Thread\n")
+        self.wsocket.send("CAP REQ :twitch.tv/commands\r\n".encode("utf-8"))
+        running = True
+        while running:
+            last = self.wsocket.recv(128).decode("utf-8")
+            recents = last.split("\r\n")
+            recents[0] = incomplete + recents[0]
+            incomplete = recents[-1]
+            del recents[-1]
+            for oline in recents:
+                sender = oline.split("!")[0][1:]
+                line = whisper.sub("", oline)
+                if line[:19] == "PING :tmi.twitch.tv":
+                    self.socket.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+                else:
+                    if not oline[:14] == ":tmi.twitch.tv" and not oline == u"" and not line == oline:
+                        if line[0:9] == "!shutdown":
+                            self.whisper(sender,"Bye bye")
+                            self.running = False
+                            running = False
+                            self.socket.shutdown(socket.SHUT_WR)
+                            self.wsocket.shutdown(socket.SHUT_WR)
+                            print("Shutting Down")
+                        elif "hola" in line:
+                            self.whisper(sender,"te amo")
+                        try:
+                            print("#" + sender, "-", line)
+                        except UnicodeError:
+                            print("~~## ERROR LINE =( ##~~")
+            time.sleep(0.01)
+
+
     def chat(self,message):
         self.socket.send("PRIVMSG #{} :{}\r\n".format(self.channel, message).encode("utf-8"))
+
+    def whisper(self,user,message):
+        self.socket.send("PRIVMSG #{} :/w {} {}\r\n".format(self.channel, user,message).encode("utf-8"))
--- TwitchUtils.py
+++ TwitchUtils.py
@@ -1,8 +1,11 @@
-import socket
+import socket,json
+from urllib.request import urlopen
 import IxiaExceptions
-def connectToChannel(token,nick,channel):
+
+
+def connectToChannel(token,nick,channel,server=("irc.twitch.tv",6667)):
     s = socket.socket()
-    s.connect(("irc.twitch.tv",6667))
+    s.connect(server)
 
     s.send("PASS {}\r\n".format(token).encode("utf-8"))
     s.send("NICK {}\r\n".format(nick).encode("utf-8"))
@@ -12,3 +15,32 @@ def connectToChannel(token,nick,channel):
         return s
     else:
         raise IxiaExceptions.CannotConnectException(r)
+
+
+def setupWhispers(token,nick):
+    u = urlopen("http://chatdepot.twitch.tv/room_memberships?oauth_token={}".format(token[6:])).read()
+    j = json.loads(u.decode("utf-8"))
+    j = j["memberships"][0]
+    cname = j["room"]["irc_channel"]
+    servers = j["room"]["servers"]
+    aserver = ""
+    for server in servers:
+        server = server.split(":")
+        if server[1] == "6667":
+            try:
+                ws = connectToChannel(token,nick,cname,(server[0], 6667))
+            except IxiaExceptions.CannotConnectException:pass
+            else:
+                print("Connected to whisper Channel")
+                return ws
+    for server in servers:
+        server = s.split(":")
+        if server[1] == "80":
+            try:
+                ws = connectToChannel(server[0], 80, cname)
+            except IxiaExceptions.CannotConnectException:
+                pass
+            else:
+                print("Connected to whisper Channel")
+                return ws
+    return None
