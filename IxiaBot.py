32328b9e276035d3aab787a7ebc372d79d3b0b3c
Hydrox6 <ikada@protonmail.ch>
2016-07-13 02:41:00 +0100

    Messed around with formatting, added some comments.

--- IxiaBot.py
+++ IxiaBot.py
@@ -1,9 +1,10 @@
-import socket,re,time,datetime,json,urllib
+import socket,re,time
 from threading import Thread
-import TwitchUtils,IxiaExceptions
+import TwitchUtils
 
 msg = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
 whisper = re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv WHISPER \w+ :")
+
 
 class IxiaBot:
 
@@ -12,7 +13,7 @@ class IxiaBot:
         self.token = token
         self.channel = channel
         self.socket = TwitchUtils.connectToChannel(self.token, self.nick, self.channel)
-        self.wsocket = TwitchUtils.setupWhispers(self.token,self.nick)
+        self.wsocket = TwitchUtils.setupWhispers(self.token, self.nick)
         self.running = True
 
     def start(self):
@@ -21,34 +22,36 @@ class IxiaBot:
 
     def listen(self):
         incomplete = ""
+        print("Starting chat watching")
         while self.running:
-            last = self.socket.recv(128).decode("utf-8") # Reads 128 Bytes from the chat
-            recents = last.split("\r\n") # Splits it up by line(s)
-            recents[0] = incomplete + recents[0] # Adds on the previous incomplete line.
-            incomplete = recents[-1] # makes incomplete the new incomplete line
-            del recents[-1] # Makes sure the incomplete line isn't processed this cycle
-            for oline in recents:
-                #Line format is :<sender>!<sender>@<sender>.tmi.twitch.tv PRIVMSG #<channel> :<message>
-                sender = oline.split("!")[0][1:] # gets the sender
-                line = msg.sub("", oline) # Gets the message using the wonders of Regex
-                if line[:19] == "PING :tmi.twitch.tv":
+            last = self.socket.recv(128).decode("utf-8")                        # Reads 128 Bytes from the chat
+            recents = last.split("\r\n")                                        # Splits it up by line(s)
+            recents[0] = incomplete + recents[0]                                # Adds on the previous incomplete line.
+            incomplete = recents[-1]                                            # Updates what line is considered incomplete.
+            del recents[-1]                                                     # Makes sure the incomplete line isn't processed this cycle.
+            for oline in recents:                                               # Line format is :<sender>!<sender>@<sender>.tmi.twitch.tv PRIVMSG #<channel> :<message>
+                sender = oline.split("!")[0][1:]                                # Gets the sender.
+                line = msg.sub("", oline)                                       # Gets the message using the wonders of Regex.
+                if line[:19] == "PING :tmi.twitch.tv":                          # Twitch sometimes sends these keep-alives. Sends the required response.
                     self.socket.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
                 else:
-                    if not sender == self.nick and not oline[:14] == ":tmi.twitch.tv" and not oline == u"" and not line == oline:
-                        if "greetings" in line:
-                            self.chat("ohaider")
-                        try:
-                            print(sender, "-", line)
-                        except UnicodeError:
-                            print("~~## ERROR LINE =( ##~~")
-        time.sleep(0.01)
+                    if not sender == self.nick:                                 # Makes IxiaBot ignore her own messages.
+                        if not oline[:14] == ":tmi.twitch.tv":                  # Ignore automated messages from twitch when parsing commands and such.
+                            if not oline == u"":                                # Ignore empty lines (this should be a rarity.
+                                if not line == oline:                           # I'm not sure why this line is here, as this code is from an older project.
+                                    if "greetings" in line:                     # For testing purposes only.
+                                        self.chat("ohaider")
+                                    try:
+                                        print(sender, "-", line)                # Prints the lines, so we can see what the bot sees.
+                                    except UnicodeError:                        # This may or may not be important, but it's there anyways.
+                                        print("~~## ERROR LINE =( ##~~")
+        time.sleep(0.01)                                                        # Usually a good idea
 
     def whisperListen(self):
         incomplete = ""
-        print("Starting whisper Thread\n")
-        self.wsocket.send("CAP REQ :twitch.tv/commands\r\n".encode("utf-8"))
-        running = True
-        while running:
+        print("Starting whisper Thread")
+        self.wsocket.send("CAP REQ :twitch.tv/commands\r\n".encode("utf-8"))    # Requests whispers. (Required)
+        while self.running:                                                     # This is technically the same code as above, just with the whisper socket.
             last = self.wsocket.recv(128).decode("utf-8")
             recents = last.split("\r\n")
             recents[0] = incomplete + recents[0]
@@ -60,20 +63,22 @@ class IxiaBot:
                 if line[:19] == "PING :tmi.twitch.tv":
                     self.socket.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
                 else:
-                    if not oline[:14] == ":tmi.twitch.tv" and not oline == u"" and not line == oline:
-                        if line[0:9] == "!shutdown":
-                            self.whisper(sender,"Bye bye")
-                            self.running = False
-                            running = False
-                            self.socket.shutdown(socket.SHUT_WR)
-                            self.wsocket.shutdown(socket.SHUT_WR)
-                            print("Shutting Down")
-                        elif "hola" in line:
-                            self.whisper(sender,"te amo")
-                        try:
-                            print("#" + sender, "-", line)
-                        except UnicodeError:
-                            print("~~## ERROR LINE =( ##~~")
+                    if not sender == self.nick:
+                        if not oline[:14] == ":tmi.twitch.tv":
+                            if not oline == u"":
+                                if not line == oline:
+                                    if line[0:9] == "!shutdown":
+                                        self.whisper(sender,"Bye bye")
+                                        self.running = False
+                                        self.socket.shutdown(socket.SHUT_WR)
+                                        self.wsocket.shutdown(socket.SHUT_WR)
+                                        print("Shutting Down")
+                                    elif "hola" in line:
+                                        self.whisper(sender,"te amo")
+                                    try:
+                                        print("#" + sender, "-", line)
+                                    except UnicodeError:
+                                        print("~~## ERROR LINE =( ##~~")
             time.sleep(0.01)
 
 
@@ -81,4 +86,4 @@ class IxiaBot:
         self.socket.send("PRIVMSG #{} :{}\r\n".format(self.channel, message).encode("utf-8"))
 
     def whisper(self,user,message):
-        self.socket.send("PRIVMSG #{} :/w {} {}\r\n".format(self.channel, user,message).encode("utf-8"))
+        self.socket.send("PRIVMSG #{} :/w {} {}\r\n".format(self.channel, user, message).encode("utf-8"))
